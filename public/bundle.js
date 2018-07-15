038df5be58be40ea2b9e3c76df2e07eb8575a48b
ralph kemp <ralphk3mp@gmail.com>
2018-07-15 23:57:47 +0100

    dsfsf

--- public/bundle.js
+++ public/bundle.js
@@ -60,11 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 15);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ 15:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -75,4 +76,5 @@
 console.log('shememfs');
 
 /***/ })
-/******/ ]);
+
+/******/ });
