3c82e0eb16ac50a7c7a38023d873d91ed51ecbf4
ralph kemp <ralphk3mp@gmail.com>
2018-07-21 23:45:02 +0100

    added prettier, created routes file for both client and server side routing, working soon

--- src/client/client.js
+++ src/client/client.js
@@ -1,7 +1,14 @@
 // Startup point for the client side app
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import BrowserRouter from 'react-router-dom';
+// remember this is the client side, which will be using BrowserRouter
+import Routes from './Routes';
 
 
-ReactDOM.hydrate(<Home />, document.querySelector('#root'));
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+  , document.querySelector('#root'));
+
