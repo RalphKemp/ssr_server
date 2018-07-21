c121d808b72cbf22177dad65b4f15a033f3b0761
ralph kemp <ralphk3mp@gmail.com>
2018-07-21 23:46:09 +0100

    pretted up

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import Home from './components/Home';
+import React from "react";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
 
 export default () => {
   return (
@@ -8,4 +8,4 @@ export default () => {
       <Route exact path="/" component={Home} />
     </div>
   );
-}
+};
--- src/client/client.js
+++ src/client/client.js
@@ -1,14 +1,13 @@
 // Startup point for the client side app
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BrowserRouter from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import BrowserRouter from "react-router-dom";
 // remember this is the client side, which will be using BrowserRouter
-import Routes from './Routes';
-
+import Routes from "./Routes";
 
 ReactDOM.hydrate(
   <BrowserRouter>
     <Routes />
-  </BrowserRouter>
-  , document.querySelector('#root'));
-
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
