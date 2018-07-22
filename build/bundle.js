97b66a95228740c8d1ef1baa71c011a66e0139a9
ralph kemp <ralphk3mp@gmail.com>
2018-07-22 01:21:55 +0100

    delete dummy route

--- build/bundle.js
+++ build/bundle.js
@@ -215,10 +215,7 @@ exports.default = function () {
   return _react2.default.createElement(
     "div",
     null,
-    _react2.default.createElement(_reactRouterDom.Route, { exact: true, path: "/", component: _Home2.default }),
-    _react2.default.createElement(_reactRouterDom.Route, { exact: true, path: "/cheese", component: function component() {
-        return 'hi';
-      } })
+    _react2.default.createElement(_reactRouterDom.Route, { exact: true, path: "/", component: _Home2.default })
   );
 };
 
