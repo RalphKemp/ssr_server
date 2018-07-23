df199354ca0a7912031d0547acbe02e3499de949
ralph kemp <ralphk3mp@gmail.com>
2018-07-23 13:08:22 +0100

    working on redux, starting from client.js, created provider and redux store, added nessecary imports

--- src/client/client.js
+++ src/client/client.js
@@ -3,11 +3,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 // remember this is the client side, which will be using BrowserRouter
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+// thunk to handle async action creators
+import { Provider } from 'react-redux';
+// ties store, used to communicate data from store to connect components
 import Routes from "./Routes";
 
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.querySelector("#root")
 );
+
+// pass store as a prop to our provider tag, so now the provider has
+// a reference to the redux store, any time the redux store changes,
+// the provider will alert any connected componentns in the app that
+// they need to re-render.
