8b55218fbfeeb546f1c0ec59c28c1d7496b7813d
ralph kemp <ralphk3mp@gmail.com>
2018-07-23 13:56:38 +0100

    creating external store, which wiill be used for getting inital data when action creators are completed

--- src/client/client.js
+++ src/client/client.js
@@ -3,10 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 // remember this is the client side, which will be using BrowserRouter
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 // thunk to handle async action creators
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 // ties store, used to communicate data from store to connect components
 import Routes from "./Routes";
 
---
+++ src/helpers/createStore.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+export default () => {
+  const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+  return store;
+};
+
+// so obviously, normally we would use this store as a prop to the provider tag,
+// which would wrap our staticrouter tag in renderer.js.
+// But on the server, we need some way to detect when all the initial data
+// load action creators are completed.
+// we're gonna create the store in our route handler in index.js, and after we have done
+// all of our store initializtion, all the data loading inside of it, then we will pass
+// the store off to the renderder, where it can be used by the provider function.
